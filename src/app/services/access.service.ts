4d28189b40865f7475e1cc2ae1d122372b1fde50
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-06-14 16:07:24 +0200

    Aggiunte funzionalita'

--- src/app/services/access.service.ts
+++ src/app/services/access.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -8,7 +9,10 @@ import { environment } from 'src/environments/environment';
 })
 export class AccessService {
 
-  constructor(private http : HttpClient) { }
+  constructor(
+      private http : HttpClient,
+      private router : Router
+  ) { }
 
   authenticate(email: string, password: string): Observable<any> {
     const url = environment.apiUrl+"auth/authenticate";
@@ -21,5 +25,19 @@ export class AccessService {
       observe: 'response'
     },
     );
+  }
+
+  logout() {
+    sessionStorage.removeItem('accessToken');
+    this.router.navigate(['/']);
+  }
+
+  isLoggedIn(): boolean {
+    let token = sessionStorage.getItem('accessToken');
+    return token != null && token.length > 0;
+  }
+
+  getToken(): string | null {
+    return this.isLoggedIn() ? sessionStorage.getItem('accessToken') : null;
   }
 }
--- src/app/services/utenti.service.ts
+++ src/app/services/utenti.service.ts
@@ -2,18 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Utenti } from '../models/Utenti';
 import { Observable } from 'rxjs';
+import { AccessService } from './access.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtentiService {
 
-  utenti?: Utenti;
+  utenti?: Utenti[];
 
-  constructor(private http : HttpClient) { }
+  constructor(
+    private http : HttpClient,
+    private accessService : AccessService
+    ) { }
 
   getAllUtenti(): Observable<Utenti[]> {
+    let token = this.accessService.getToken();
+    const headers = { 'Authorization': 'Bearer '+token }
     const url = "http://localhost:8081/ristorante/management/utenti";
-    return this.http.get<Utenti[]>(url);
+    return this.http.get<Utenti[]>(url, {headers});
   }
 }
