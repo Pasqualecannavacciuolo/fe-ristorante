1f75829ca3438dbe49f562cc5a40c73439731115
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-08-28 16:34:35 +0200

    Migliorata la funzionalita'  del DELETE

--- src/app/shared/components/table/actiontable/actiontable.component.ts
+++ src/app/shared/components/table/actiontable/actiontable.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Utenti } from 'src/app/models/Utenti';
 import { UtentiService } from 'src/app/services/utenti.service';
 
@@ -14,20 +14,28 @@ export class ActiontableComponent implements OnInit {
 
   constructor(
     private router : Router,
-    private route : ActivatedRoute,
     private utentiService : UtentiService
     ) {}
 
   ngOnInit(): void {
   }
 
+  //Funzione che permette di ricaricare l'url corrente dopo aver effettato un'operazione
+  reloadCurrentRoute() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['/home/',{ outlets: { dashboard: ['utenti'] } }]);
+    });
+  }
+
+  // Funzione che effettua il redirect alla pagina dove effettuare l'update dell'utente
   redirectToUpdateUtente(user: Utenti) : void {
     this.router.navigate(['/home/',{ outlets: { dashboard: ['updateUtente', user.id] } }]);
   }
 
+  // Funzione che permette l'eliminazione di un utente
   deleteUtente(userId : number) : void {
-    this.utentiService.deleteUtente(userId).subscribe(res => {
-      console.log(JSON.stringify(res))
+    this.utentiService.deleteUtente(userId).subscribe(() => {
+      this.reloadCurrentRoute();
     });
   }
 }
